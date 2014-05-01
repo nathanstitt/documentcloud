d0c8e531f7aefc16afeea534f529daf613aa7f54
Nathan Stitt <nathan@stitt.org>
2014-05-01 11:23:19 -0500

    Prep for testing cloud-crowd commit on staging

---
+++ app/actions/db_fuzz.rb
@@ -0,0 +1,20 @@
+require File.dirname(__FILE__) + '/support/setup'
+
+# A simple job for testing CloudCrowd's performace
+# It's designed to perform minimal work, and exit
+# quickly
+class DbFuzz < CloudCrowd::Action
+
+  def process
+
+    doc_id = rand(Document.maximum(:id) )
+    if ( document = Document.where({ id: doc_id  }).first )
+      document.pages.pluck(:page_number).last
+      Rails.logger.info "Page #{page_number}"
+    end
+    Rails.logger.info "Document #{doc_id} found: #{!document.nil?}"
+    sleep 2
+    true
+  end
+
+end
