25213288c12851b7746de92ba41f643339cb6131
Nathan Stitt <nathan@stitt.org>
2014-06-03 11:38:27 -0500

    Controller for data access

--- app/controllers/server_status_controller.rb
+++ app/controllers/server_status_controller.rb
@@ -1,12 +1,18 @@
 class ServerStatusController < ApplicationController
 
   before_action :prefer_secure
-  before_action :current_account
+  before_action :current_account, :except=> :data
 
   layout 'home'
 
   def index
+    @stylesheets=[:server_status]
     @javascripts=[:server_status]
+  end
+
+  def data
+    rows = [ rand(10), rand(10) ]
+    render :json=>rows
   end
 
 end
--- app/models/cloud_crowd_stat.rb
+++ app/models/cloud_crowd_stat.rb
@@ -1,14 +1,16 @@
 class CloudCrowdStat < ActiveRecord::Base
 
 
-  def self.record( interval )
+  scope :recent, -> { where('lower(period) < ?',Time.now-1.day) }
+
+  def self.record(interval)
     CloudCrowd::Job.class_eval do
       config_path = "./config/cloud_crowd/#{Rails.env}/database.yml"
       configuration = YAML.load(ERB.new(File.read(config_path)).result)
       self.establish_connection(configuration)
     end
 
-    stat = CloudCrowdStat.new({ :period=>interval })
+    stat = CloudCrowdStat.new({:period=>interval})
 
     stat.pending_count = CloudCrowd::Job.incomplete.count
     stat.average_wait  = CloudCrowd::Job.complete
@@ -19,7 +21,6 @@ class CloudCrowdStat < ActiveRecord::Base
 
     stat.save!
   end
-
 
 
 end
