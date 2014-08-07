1d84b025d3d554279643afbae125643ee6e180be
Nathan Stitt <nathan@stitt.org>
2014-08-07 17:43:34 -0500

    Move retry block to SolrTask, add comments

--- app/models/solr_task.rb
+++ app/models/solr_task.rb
@@ -1,32 +1,40 @@
 class SolrTask < ActiveRecord::Base
 
+  MAX_TRIES = 3 # How many times a task should be re-tried before giving up
+
   belongs_to :record, polymorphic: true
 
-
-  def self.index_later(record, options={})
-    SolrTask.create!({ :record=>record, 'options'=>options })
-    
+  def retry
+    self.update_attributes(:attempts, self.attempts+1)
+    if options['removal']
+      # Here we have to be careful.  The record won't be found since it was a delete.
+      # this is taken from the Sunspot::Rails::Searchable#solr_clean_index_orphans
+      new self.record_type do | fake_instance |
+        fake_instance.id = self.record_id
+        fake_instance.solr_remove_from_index
+      end
+    else
+      self.record.solr_index
+    end
+  rescue Errno::ECONNREFUSED, Net::ReadTimeout
+    if self.attempts < MAX_TRIES
+      self.update_attributes(:failed=>true)
+    end
   end
 
-  def self.perform
-    SolrTask.where(:failed=>false).find_each do | job |
-      job.update_attributes(:attempts, job.attempts+1)
-      begin
-        if options['removal']
-          # Here we have to be careful.  The record won't be found
-          # this is takenf rom the Sunspot::Rails::Searchable#solr_clean_index_orphans
-          new job.record_type do | fake_instance |
-            fake_instance.id = job.record_id
-            fake_instance.solr_remove_from_index
-          end
-        else
-          job.record.solr_index
-        end
-        job
-      rescue Errno::ECONNREFUSED, Net::ReadTimeout
-        if job.attempts < MAX_TRIES
-          job.update_attributes(:failed=>true)
-        end
-      end
-    end
+
+
+  # If any of the common Solr exceptions occur,
+  # save the record's type and ID and retry the indexing later
+  def self.perform(record, options={}, &block)
+    yield
+    rescue Errno::ECONNREFUSED, Net::ReadTimeout
+      SolrTask.create!({ :record=>record, 'options'=>options })
+  end
+
+  # Loops through
+  def self.retry_failures
+    SolrTask.where(:failed=>false).find_each{ | job | job.retry }
+  end
+
 end
--- lib/dc/search/model_support.rb
+++ lib/dc/search/model_support.rb
@@ -1,29 +1,46 @@
 module DC
   module Search
 
+    # DC::Search::ModelSupport extends Solr search
+    # to include failure handling and background retry
     module ModelSupport
+
       def self.included(base)
         base.module_eval do
-          extend(ClassMethods)
+          extend(SchemaDefinition)
         end
       end
 
-      module ClassMethods
+      module SchemaDefinition
+
+        # Calls Sunspot::Rails::Searchable#searchable method
+        # with auto_index and auto_remove set to false,
+        # and then sets up our own handlers for those cases
         def searchable_with_deferred_indexing(options = {}, &block)
-          include InstanceMethods
-          options[:auto_index]=false
+          include SafeIndexing
+          options.merge!(:auto_index=>false,:auto_remove=>false)
           searchable(options,&block)
-          before_save :mark_for_auto_indexing_or_removal
-          after_save  :ensure_indexed
+          # mark_for_auto_indexing_or_removal is defined by
+          # Sunspot::Rails.  It checks for if/unless and sets
+          # up the perform_index_tasks method
+          before_save   :mark_for_auto_indexing_or_removal
+          after_save    :ensure_indexed
+          after_destroy :ensure_index_removed
         end
 
       end
 
-      module InstanceMethods
+      # Methods are included by the searchable_with_deferred_indexing
+      # method.  Consists of callback handlers for after_save and after_destroy
+      module SafeIndexing
+
+        # Wraps the indexing into a task so it can be re-tried if it fails
         def ensure_indexed
-          perform_index_tasks
-        rescue Errno::ECONNREFUSED, Net::ReadTimeout
-          SolrTask.index_later(self, :removal=>@marked_for_auto_removal)
+          SolrTask.perform(self){ perform_index_tasks }
+        end
+
+        def ensure_index_removed
+          SolrTask.perform(self,:removal=>true){ solr_remove_from_index }
         end
       end
 
